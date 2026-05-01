fb53b518eeeb90ff3978d51c89baf97fdc916fd1
Muhammad Naufal Musthofa <naufal@Muhammads-MacBook-Air.local>
2026-05-01 13:07:22 +0700

    Increase billing upload limit and improve payment notification

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typedRoutes: true
+  typedRoutes: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "6mb"
+    }
+  }
 };
 
 export default nextConfig;
